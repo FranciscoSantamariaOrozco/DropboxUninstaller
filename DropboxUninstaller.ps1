cbaa587d21787dc73e2540459a6335ac459c9590
Francisco Orozco ActiveIT-ThinkPad <francisco.s.orozco@outlook.com>
2023-03-03 10:16:30 +0000

    Improve with detector.

---
+++ DropboxUninstaller.ps1
@@ -0,0 +1,4 @@
+# Autor: Francisco Orozco
+
+$ruta = "C:\Program Files (x86)\Dropbox\Client\DropboxUninstaller.exe"
+Start-Process $ruta /S -Wait
---
+++ DropboxUninstallerDetector.ps1
@@ -0,0 +1,10 @@
+$uninstallerName = "DropboxUninstaller.exe"
+
+$uninstallers = Get-ChildItem "C:\Program Files (x86)\", "C:\Program Files\" -Include $uninstallerName -Recurse -ErrorAction SilentlyContinue
+
+
+if ($uninstallers.Count -gt 0) {
+    Start-Process $uninstallers.FullName /S -Wait
+} else {
+    exit
+}
